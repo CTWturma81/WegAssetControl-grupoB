6d547fea2aca665b62838583fd94de09eeb0bb67
Davi Heinzen <heinzen08moraes@gmail.com>
2026-06-26 15:12:24 -0300

    feat(usuarioService): implementa metodo BuscarPorId (#69)
    * feat(UsuarioService): cria a service
    
    * chore: remove .idea do tracking
    
    * feat(sensor): implementa entidade sensor
    
    * feat(ativo): implementa entidade AtivoIndustrial (#61)
    
    * feat(exceptions): torna exceptions filhas da AppExceptions (#54)
    
    * feat(exceptions): implementa exceções personalizadas
    
    * fix(exceptions): torna excecoes filhas da AppException
    
    ---------
    
    Co-authored-by: CAMILE WENGRZYNEK <camile_wengrzynek@estudante.sesisenai.org.br>
    
    * feat(ativo): implementa entidade AtivoIndustrial
    
    * feat(ativo): implementa entidade AtivoIndustrial
    
    * chore: remove .idea from tracking
    
    * feat(ativo): implementa entidade AtivoIndustrial
    
    ---------
    
    Co-authored-by: CAMILE WENGRZYNEK <camile_wengrzynek@estudante.sesisenai.org.br>
    Co-authored-by: CHRISTIAN FACHINI <christian_fachini@estudante.sesisenai.org.br>
    
    * feat(ativo): implementa entidade AtivoIndustrial (#62)
    
    * feat(exceptions): torna exceptions filhas da AppExceptions (#54)
    
    * feat(exceptions): implementa exceções personalizadas
    
    * fix(exceptions): torna excecoes filhas da AppException
    
    ---------
    
    Co-authored-by: CAMILE WENGRZYNEK <camile_wengrzynek@estudante.sesisenai.org.br>
    
    * feat(ativo): implementa entidade AtivoIndustrial
    
    * feat(ativo): implementa entidade AtivoIndustrial
    
    * chore: remove .idea from tracking
    
    * feat(ativo): implementa entidade AtivoIndustrial
    
    ---------
    
    Co-authored-by: CAMILE WENGRZYNEK <camile_wengrzynek@estudante.sesisenai.org.br>
    Co-authored-by: CHRISTIAN FACHINI <christian_fachini@estudante.sesisenai.org.br>
    
    * feat(Usuario): implementa metodo buscarPorId na service
    
    ---------
    
    Co-authored-by: joaquim_pimenta <joaquim_pimenta@estudante.sesisenai.org.br>
    Co-authored-by: artQT <artur_thizon@estudante.sesisenai.org.br>
    Co-authored-by: CAMILE WENGRZYNEK <camile_wengrzynek@estudante.sesisenai.org.br>
    Co-authored-by: CHRISTIAN FACHINI <christian_fachini@estudante.sesisenai.org.br>

---
+++ src/service/UsuarioService.java
@@ -0,0 +1,84 @@
+package service;
+
+import enums.PerfilAcesso;
+import exception.AppException;
+import model.entity.Usuario;
+import model.repository.UsuarioRepository;
+
+import java.util.Collection;
+
+
+public class UsuarioService {
+
+    private UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository){
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public Usuario cadastrarUsuario(Usuario usuario){
+
+        if(usuarioRepository.buscarPorLogin(usuario.getLogin()) != null){
+            throw new AppException("ERRO: Login já está em uso.");
+        }
+        return usuarioRepository.criarUsuario(usuario);
+    }
+
+    public Collection<Usuario> listarUsuarios(){
+        Collection<Usuario> usuarios = usuarioRepository.listarUsuarios();
+
+        if(usuarios.isEmpty()){
+            throw new AppException("ERRO: Nenhum usuário cadastrado.");
+        }
+
+        return usuarios;
+    }
+
+    public Usuario atualizarUsuario(Integer id, Usuario novoUsuario){
+        Usuario usuario = usuarioRepository.buscarPorId(id);
+
+        if(usuario == null){
+            throw  new AppException("ERRO: Usuário não encontrado.");
+        }
+
+        if(!usuario.getLogin().equals(novoUsuario.getLogin()) && usuarioRepository.buscarPorLogin(novoUsuario.getLogin()) != null){
+            throw new AppException("ERRO: Login já está em uso.");
+        }
+
+        return usuarioRepository.atualizarUsuario(id ,novoUsuario);
+    }
+
+    public void inativarUsuario(Integer id){
+        Usuario usuario = usuarioRepository.buscarPorId(id);
+
+        if(usuario == null){
+            throw new AppException("ERRO: Usuário não encontrado.");
+        }
+
+        if(usuario.getPerfil() == PerfilAcesso.ADMINISTRADOR){
+            int totalAdminsAtivos = 0;
+            for(Usuario u : usuarioRepository.listarUsuarios()){
+                if(u.getPerfil() == PerfilAcesso.ADMINISTRADOR && u.getAtivo()){
+                    totalAdminsAtivos++;
+                }
+            }
+
+            if(totalAdminsAtivos <= 1){
+                throw new AppException("ERRO: Precisa de pelo menos um administrador ativo no sistema");
+            }
+        }
+
+        usuario.setAtivo(false);
+    }
+
+    public Usuario buscarPorId(Integer id){
+        Usuario usuario = usuarioRepository.buscarPorId(id);
+
+        if(usuario == null){
+            throw new AppException("ERRO: Usuario não encontrado.");
+        }
+
+        return usuario;
+    }
+
+}
