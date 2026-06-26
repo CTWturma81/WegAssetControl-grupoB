4d14d5c5ae6403e7f76c51293fe044d15d95de36
Davi Heinzen <heinzen08moraes@gmail.com>
2026-06-26 16:46:32 -0300

    feat(sensor): implementa SensorRepository (#78)
    * feat(exceptions): torna exceptions filhas da AppExceptions (#54)
    
    * feat(exceptions): implementa exceções personalizadas
    
    * fix(exceptions): torna excecoes filhas da AppException
    
    ---------
    
    Co-authored-by: CAMILE WENGRZYNEK <camile_wengrzynek@estudante.sesisenai.org.br>
    
    * feat(ativo): implementa entidade AtivoIndustrial (#56)
    
    Co-authored-by: CHRISTIAN FACHINI <christian_fachini@estudante.sesisenai.org.br>
    
    * feature(sensor): implementação do sensorRepository
    
    ---------
    
    Co-authored-by: CAMILE WENGRZYNEK <camile_wengrzynek@estudante.sesisenai.org.br>
    Co-authored-by: CHRISTIAN FACHINI <christian_fachini@estudante.sesisenai.org.br>
    Co-authored-by: artQT <artur_thizon@estudante.sesisenai.org.br>

---
+++ src/model/repository/SensorRepository.java
@@ -0,0 +1,47 @@
+package model.repository;
+
+import model.entity.AtivoIndustrial;
+import model.entity.Sensor;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class SensorRepository {
+
+    Map<Integer, Sensor> sensorMap = new HashMap<>();
+    private static Integer idcontador = 1;
+
+    public void salvar(Sensor sensor){
+        sensorMap.put(idcontador++,sensor);
+    }
+
+    public Sensor buscarPorId(int id){
+        return sensorMap.get(id);
+    }
+
+    public Sensor buscarPorCodigo(Sensor sensor){
+        return sensorMap.get(sensor.getCodigo());
+    }
+
+    public Collection<Sensor> listarTodos(){
+        return sensorMap.values();
+    }
+
+    public Collection<Sensor> listarPorAtivo(AtivoIndustrial ativoIndustrial) {
+        return sensorMap.values().stream()
+                .filter(sensor -> sensor.getAtivoIndustrial() != null)
+                .filter(sensor -> Objects.equals(sensor.getAtivoIndustrial(), ativoIndustrial))
+                .collect(Collectors.toSet());
+    }
+
+    public Sensor Atualizar(Integer id,Sensor sensor){
+        if(sensorMap.containsKey(id)){
+            return sensorMap.replace(id,sensor);
+        }else{
+            return null;
+        }
+    }
+}
